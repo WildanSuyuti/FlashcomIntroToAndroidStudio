ac46f632d6ad481376d8932c1b249565cdfd1c72
Muhammad Wildan Suyuti <=>
2018-08-07 20:21:44 +0700

    initial commit

---
+++ app/src/main/java/id/co/flashcome/introandroidstudio/main/MainActivity.java
@@ -0,0 +1,15 @@
+package id.co.flashcome.introandroidstudio.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.co.flashcome.introandroidstudio.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
